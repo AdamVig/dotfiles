589b9a16c008123e874e381e37a7ba0db0a9e85a
Adam Vigneaux <adamvigneaux@gmail.com>
2016-05-16 11:29:52 -0400

    Add brew commands and config

---
+++ brew.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Refresh homebrew
+brew update
+brew upgrade
+
+# Command line upgrades
+brew install bash
+brew install git
+brew install zsh
+brew install oh-my-zsh
+
+# Utilities
+brew install cheat
+brew install emacs
+brew install httpie
+brew install ssh-copy-id
+brew install z
+
+# Languages
+brew install node
+brew install python3
+
+# Applications
+brew cask install flux
+brew cask install google-chrome
+brew cask install spotify
