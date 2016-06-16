cb392a3653ee1b142a6f1b881ca8576c5bdb2477
Adam Vigneaux <adamvigneaux@gmail.com>
2016-06-15 20:03:04 -0400

    Install Node 6.x instead of 4.x

--- apt.sh
+++ apt.sh
@@ -8,11 +8,11 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # Install Node.js and npm
-sudo apt-get remove --purge node  # Remove old version
-curl -sL https://deb.nodesource.com/setup | sudo bash -  # Add repositories
+sudo apt-get remove --purge node  # Remove old version if installed
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -  # Add repo
 sudo apt-get install -y nodejs
 sudo apt-get install -y npm
-sudo ln -s /usr/bin/nodejs /usr/bin/node  # Make the binary available as just "node"
+sudo ln -s /usr/bin/nodejs /usr/bin/node  # Make the binary available as "node"
 
 # Configure npm global package directory
 mkdir ~/.npm-global
