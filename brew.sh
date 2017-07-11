40eb14836bc9917e268143302012ab21b458aa93
Adam Vigneaux <adam@adamvig.com>
2017-07-11 15:56:22 -0400

    Add color to homebrew script output

--- brew.sh
+++ brew.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Colorized output
+# $1: string to print, must be quoted
+# $2: optional, name of color, defaults to magenta
+function message() {
+    MAGENTA="\e[35m"
+    DEFAULT="\e[0m"
+    printf "$MAGENTA%s$DEFAULT\n" "$1"
+}
+
+message "Setting up Homebrew..."
+
 declare -a brew_formulas=(
     # Upgrade default command line tools
     bash
@@ -41,44 +52,47 @@ declare -a cask_font_formulas=(
 )
 
 if [ ! -f "`which brew`" ]; then
-    printf "%s\n" "Installing Homebrew..."
+    message "  Installing Homebrew..."
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-    printf "%s\n" "Tapping caskroom/fonts..."
+    message "  Tapping caskroom/fonts..."
     brew tap caskroom/fonts
 fi
 
 # Check if directory is writable, if not, take ownership of it
 if [ ! -w /usr/local ]; then
-    printf "%s\n" "Taking ownership of /usr/local..."
+    message "  Taking ownership of /usr/local..."
     sudo chown -R "$(whoami)" /usr/local
 fi
 
-printf "%s\n" "Updating Homebrew package lists..."
+message "  Updating Homebrew package lists..."
 brew update &> /dev/null
 
-printf "%s\n" "Upgrading installed packages..."
+message "  Upgrading installed packages..."
 brew upgrade &> /dev/null
 
-printf "%s\n" "Installing Brew formulas..."
+message "  Installing Brew formulas..."
 
 # Install Brew formulas, suppress "already installed" warnings
 for formula in "${brew_formulas[@]}"; do
     brew install "$formula" 2> /dev/null
+    message "    Installed $formula"
 done
 
-printf "%s\n" "Installing Brew Cask formulas..."
+message "  Installing Brew Cask formulas..."
 
 # Install Cask formulas, suppress "already installed" warnings
 for formula in "${cask_formulas[@]}"; do
     brew cask install "$formula" 2> /dev/null
+    message "    Installed $formula"
 done
 
-printf "%s\n" "Installing Brew Cask font formulas..."
+message "  Installing Brew Cask font formulas..."
 
 # Install Cask font formulas, suppress "already installed" warnings
 for formula in "${cask_font_formulas[@]}"; do
     brew cask install "$formula" 2> /dev/null
+    message "    Installed $formula"
 done
 
-printf "%s\n" "Brew done."
+message "Homebrew done."
