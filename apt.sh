8edabc4660ff6f7da186bf56e4f57964e88f572f
Adam Vigneaux <adam@adamvig.com>
2017-07-26 09:36:31 -0400

    Update Node version on Linux

--- apt.sh
+++ apt.sh
@@ -6,9 +6,8 @@ sudo apt-get upgrade -y
 
 # Install Node.js and npm
 sudo apt-get remove --purge node  # Remove old version if installed
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -  # Add repo
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo apt-get install -y npm
 sudo ln -s /usr/bin/nodejs /usr/bin/node  # Make the binary available as "node"
 
 # Configure npm global package directory
