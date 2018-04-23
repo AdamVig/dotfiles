c0cc771dabe35bf7af4c13f9e9470d06a4c1f853
Adam Vigneaux <adam@adamvig.com>
2018-04-23 09:53:16 -0400

    Switch from manual Brew package installation to Brewfile
    This allows for simpler specifications of Brew packages and for advanced
    features like installing packages with flags.

---
+++ Brewfile
@@ -0,0 +1,60 @@
+tap "caskroom/drivers"
+tap "caskroom/fonts"
+
+# default command line tools
+brew "bash"
+brew "curl"
+brew "emacs"
+brew "git"
+brew "gnupg"
+brew "grep", args: ["with-default-names"]
+brew "wget"
+brew "zsh"
+
+# languages
+brew "go"
+brew "python"
+brew "ruby"
+
+# utilities
+brew "docker"
+brew "gifsicle" # Gif tool
+brew "grv" # Git Repository Viewer
+brew "hub", args: ["devel"] # GitHub CLI
+brew "jq" # JSON processor
+brew "mosh" # Mobile shell
+brew "nodenv" # Node environment manager
+brew "nodenv/nodenv/nodenv-default-packages" # Plugin for auto-installing list of npm packages
+brew "nodenv/nodenv/nodenv-package-rehash" # Plugin for auto-rehashing when a global package is installed or uninstalled
+brew "postgresql", restart_service: :changed
+brew "shellcheck" # Shell script linter
+brew "ssh-copy-id"
+brew "z"
+
+# applications
+cask "docker"
+cask "firefox"
+cask "google-backup-and-sync"
+cask "google-chrome"
+cask "gpg-suite"
+cask "kap"
+cask "keybase"
+cask "lastpass"
+cask "qlmarkdown"
+cask "signal"
+cask "sketch"
+cask "spectacle"
+cask "spotify"
+cask "standard-notes"
+cask "visual-studio-code"
+
+# drivers
+cask "caskroom/drivers/logitech-options"
+
+# fonts
+cask "caskroom/fonts/font-blokk-neue"
+cask "caskroom/fonts/font-fira-code"
+cask "caskroom/fonts/font-lato"
+cask "caskroom/fonts/font-noto-sans"
+cask "caskroom/fonts/font-open-sans"
+cask "caskroom/fonts/font-source-code-pro"
--- brew.sh
+++ brew.sh
@@ -12,73 +12,10 @@ sudo --validate
 
 message "Setting up Homebrew..." "$MAGENTA"
 
-declare -a brew_formulas=(
-    # Upgrade default command line tools
-    bash
-    curl
-    git
-    gpg
-    wget
-    zsh
-
-    # Utilities
-    colordiff
-    emacs
-    grv    # Git Repository Viewer
-    hub
-    jq    # JSON processor
-    markdown
-    mobile-shell
-    nodenv    # Node environment manager
-    nodenv/nodenv/nodenv-default-packages    # Plugin for auto-installing list of npm packages
-    nodenv/nodenv/nodenv-package-rehash    # Plugin for auto-rehashing when a global package is installed or uninstalled
-    shellcheck    # Shell script linter
-    ssh-copy-id
-    z
-
-    # Languages
-    golang
-    python
-    python3
-)
-
-declare -a cask_formulas=(
-    # applications
-    firefox
-    google-chrome
-    gpg-suite    # Add GPG keys to macOS Keychain
-    kap    # Screen recorder
-    keybase
-    lastpass
-    qlmarkdown    # Markdown rendering in Finder quick look
-    signal    # Encrypted messaging
-    sketch
-    spectacle    # Window manager
-    spotify
-    standard-notes
-    visual-studio-code
-
-    # fonts
-    font-blokk-neue
-    font-fira-code
-    font-lato
-    font-noto-sans
-    font-open-sans
-    font-source-code-pro
-
-    # drivers
-    logitech-options
-)
-
 if [ ! -f "$(which brew)" ]; then
     message "  Installing Homebrew..." "$MAGENTA"
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-
-message "  Tapping caskroom/fonts..." "$MAGENTA"
-brew tap caskroom/fonts &> /dev/null
-message "  Tapping caskroom/drivers..." "$MAGENTA"
-brew tap caskroom/drivers &> /dev/null
 
 # Check if directory is writable, if not, take ownership of it
 message "  Checking ownership of subdirectories of /usr/local..." "$MAGENTA"
@@ -92,26 +29,9 @@ done
 message "  Updating Homebrew package lists..." "$MAGENTA"
 brew update &> /dev/null
 
-message "  Upgrading installed packages..." "$MAGENTA"
-brew upgrade 1> /dev/null
+message "  Updating Brew packages and installing Brew formulas..." "$MAGENTA"
 
-message "  Installing Brew formulas..." "$MAGENTA"
-
-# Install Brew formulas, suppress "already installed" warnings
-for formula in "${brew_formulas[@]}"; do
-    brew install "$formula" &> /dev/null && \
-        message "    Installed $formula" "$MAGENTA" || \
-        warn "formula $formula failed to install"
-done
-
-message "  Installing Brew Cask formulas..." "$MAGENTA"
-
-# Install Cask formulas, suppress "already installed" warnings
-for formula in "${cask_formulas[@]}"; do
-    brew cask install "$formula" &> /dev/null && \
-        message "    Installed $formula" "$MAGENTA" || \
-        warn "formula $formula failed to install"
-done
+brew bundle --all
 
 message "Homebrew done." "$MAGENTA"
 
