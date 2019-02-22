0401734e60c756e5c0a912129a581ff105793d3d
Adam Vigneaux <adam@adamvig.com>
2019-02-22 16:53:35 -0500

    Add tmux configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,6 +18,7 @@ ln -sf "$DIR/.exports" ~
 ln -sf "$DIR/.functions" ~
 ln -sf "$DIR/.git-template" ~
 ln -sf "$DIR/.profile" ~
+ln -sf "$DIR/.tmux.conf" ~
 ln -sf "$DIR/.zprofile" ~
 
 if is-wsl; then
