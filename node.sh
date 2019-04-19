b7378ca09198bfd0b185a4e5c0e32ff796a1a0b7
Adam Vigneaux <adam@adamvig.com>
2019-04-19 12:52:08 -0400

    Add npm-merge-driver

--- node.sh
+++ node.sh
@@ -20,15 +20,18 @@ cp "$DIR/npm-default-packages" "$(nodenv root)/default-packages" &> /dev/null ||
 # 2. Filter to only get normal Node versions, like "  9.5.0"
 # 3. Take last line of output which will be latest version
 # 4. Remove the leading spaces
-LATEST_NODE_VERSION="$(nodenv install --list | grep '^\s\s[0-9]' | tail -1 | xargs)"
-if [[ -n "$LATEST_NODE_VERSION" ]]; then
-    message "  %s" "Installing Node $LATEST_NODE_VERSION and default npm packages..."
-    # Feed "yes" to the command in case it prompts to reinstall
-    yes | nodenv install "$LATEST_NODE_VERSION" &> /dev/null || true
-    nodenv global "$LATEST_NODE_VERSION" &> /dev/null
+latest_node_version="$(nodenv install --list | grep '^\s\s[0-9]' | tail -1 | xargs)"
+if [ -n "$latest_node_version" ]; then
+    message "  %s" "Installing Node $latest_node_version and default npm packages..."
+    nodenv install --force "$latest_node_version" &> /dev/null || true
+    nodenv global "$latest_node_version" &> /dev/null
     message "  %s" "Done installing Node and default npm packages."
 else
     warn "  %s" "could not get latest Node version; installation failed"
 fi
+
+message "  %s" "Initializing npm-merge-driver..."
+npx npm-merge-driver install --global
+message "  %s" "Done initializing npm-merge-driver."
 
 message "Done setting up Node."
