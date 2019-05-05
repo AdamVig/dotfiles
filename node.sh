5f68d6d627c9cd3f36c4ab5fdece1a34c9063f31
Adam Vigneaux <adam@adamvig.com>
2019-05-05 18:45:33 -0400

    Add error handling and Nodenv init to Node setup

--- node.sh
+++ node.sh
@@ -11,27 +11,32 @@ source "$DIR/helpers.sh"
 # Get script directory (allows running from outside `dotfiles` dir)
 DIR="$( cd "$(dirname "$0")" || return; pwd -P )"
 
-message "Setting up Node..."
+message "setting up Node..."
 
-message "  %s" "Copying default packages file..."
+message "  %s" "copying default packages file..."
 cp "$DIR/npm-default-packages" "$(nodenv root)/default-packages" &> /dev/null || true
 
 # 1. Get list of Node versions
 # 2. Filter to only get normal Node versions, like "  9.5.0"
 # 3. Take last line of output which will be latest version
 # 4. Remove the leading spaces
-latest_node_version="$(nodenv install --list | grep '^\s\s[0-9]' | tail -1 | xargs)"
+latest_node_version="$(nodenv install --list | grep -E '^\s\s[0-9\.]+$' | tail -1 | xargs)"
 if [ -n "$latest_node_version" ]; then
-    message "  %s" "Installing Node $latest_node_version and default npm packages..."
+    message "  %s" "installing Node $latest_node_version and default npm packages..."
     nodenv install --force "$latest_node_version" &> /dev/null || true
-    nodenv global "$latest_node_version" &> /dev/null
-    message "  %s" "Done installing Node and default npm packages."
+    nodenv global "$latest_node_version" || true
+    eval "$(nodenv init -)"
+    message "  %s" "done installing Node and default npm packages."
 else
-    warn "  %s" "could not get latest Node version; installation failed"
+    warn "could not get latest Node version; installation failed"
 fi
 
-message "  %s" "Initializing npm-merge-driver..."
-npx npm-merge-driver install --global
-message "  %s" "Done initializing npm-merge-driver."
+message "  %s" "initializing npm-merge-driver..."
+if command -v npx > /dev/null; then
+  npx npm-merge-driver install --global > /dev/null
+  message "  %s" "done initializing npm-merge-driver."
+else
+  warn "npx not found, could not initialize npm-merge-driver"
+fi
 
-message "Done setting up Node."
+message "done setting up Node."
