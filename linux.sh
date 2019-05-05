0e25c16b97f5762f1d3552cdd5e60fd355885e40
Adam Vigneaux <adam@adamvig.com>
2019-05-05 18:47:57 -0400

    Suppress output from bash_profile in linux.sh

--- linux.sh
+++ linux.sh
@@ -28,7 +28,8 @@ if ! command -v brew &> /dev/null; then
     message "    %s" "done installing Linuxbrew."
 fi
 
-source "$DIR/.bash_profile"
+# this will have errors but should at least initialize Linuxbrew
+source "$DIR/.bash_profile" &> /dev/null
 
 if is-wsl; then
   message "    %s" "installing ssh-agent-wsl..."
