5d4714c2a3049dfe47be6ad8598be971b481ff18
Adam Vigneaux <adam@adamvig.com>
2019-05-15 17:41:34 -0400

    Copy key bindings from Oh My Zsh

--- zsh.sh
+++ zsh.sh
@@ -33,6 +33,7 @@ fi
 message "Initializing Zsh configuration..."
 ln -sf "$DIR/.directories.zsh" ~
 ln -sf "$DIR/.git.zsh" ~
+ln -sf "$DIR/.key-bindings.zsh" ~
 ln -sf "$DIR/.zprofile" ~
 ln -sf "$DIR/.zshrc" ~
 message "Done initializing Zsh configuration."
