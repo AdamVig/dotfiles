50aadeacf98a2ed46550602e8cd4d0af4e478c38
Adam Vigneaux <adam@adamvig.com>
2019-05-18 11:03:14 -0500

    Add automatic Git configuration update
    `git.sh` will now automatically update the user's existing Git
    configuration without clobbering any of the user's custom settings.

--- git.sh
+++ git.sh
@@ -8,6 +8,8 @@ source "$DIR/helpers.sh"
 # shellcheck source=./.exports
 source "$DIR"/.exports
 
+message "setting up Git..."
+
 config_path="${XDG_CONFIG_HOME:-"$HOME"/.config}/git"
 
 if [ -e "$HOME"/.gitconfig ]; then
@@ -16,25 +18,43 @@ if [ -e "$HOME"/.gitconfig ]; then
   mv "$HOME"/.gitconfig "$config_path"/config
 fi
 
-message "Setting up Git..."
 # If gitconfig does not exist already, create one
-if [ ! -e "$config_path"/config ]; then
-    message "  %s" "Copying gitconfig to '$config_path/config'..."
+if ! [ -e "$config_path"/config ]; then
+    message "  %s" "copying gitconfig to '$config_path/config'..."
     cp "$DIR"/.gitconfig "$config_path"/config
     
-    message "  %s" "Configuring Git..."
-    read -r -p "  Full name: " NAME
-    read -r -p "  Email address: " EMAIL
-    read -r -p "  Github username: " GITHUB
+    message "  %s" "configuring Git user..."
+    read -r -p "  Full name: " name
+    read -r -p "  Email address: " email
+    read -r -p "  GitHub username: " github
 
     # Only set values if they are non-empty
-    [ -n "$NAME" ] && git config --global user.name "$NAME"
-    [ -n "$EMAIL" ] && git config --global user.email "$EMAIL"
-    [ -n "$GITHUB" ] && git config --global github.user "$GITHUB"
+    [ -n "$name" ] && git config --global user.name "$name"
+    [ -n "$email" ] && git config --global user.email "$email"
+    [ -n "$github" ] && git config --global github.user "$github"
+    message "  %s" "done configuring Git user"
 
     if "$DIR"/bin/is-wsl; then
+      message "  %s" "setting credential helper for WSL..."
       git config --global credential.helper \
         '/c/Program\ Files/Git/mingw64/libexec/git-core/git-credential-manager.exe'
     fi
+else
+  message "  %s" "updating Git configuration..."
+  # Read all values from current Git configuration
+  git config --list --file "$DIR"/.gitconfig | while read -r conf_line; do
+    # Split line into name and value at equals sign
+    IFS='=' read -r -a name_value <<< "$conf_line"
+    name="${name_value[0]}"
+    value="${name_value[1]}"
+
+    # If this configuration key is not already set, set it
+    if ! git config "$name" &> /dev/null; then
+      message "    %s" "setting '$name'..."
+      git config "$name" "$value"
+    fi
+  done
+  message "  %s" "done updating Git configuration"
 fi
-message "Done setting up Git."
+
+message "done setting up Git"
