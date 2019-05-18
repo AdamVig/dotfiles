718851fff76d274a842602f948987d6ba47a1e57
Adam Vigneaux <adam@adamvig.com>
2019-05-17 20:04:07 -0400

    Move Git configuration to XDG config home

--- git.sh
+++ git.sh
@@ -5,11 +5,22 @@ DIR="$(dirname "$(realpath "$0")")"
 # shellcheck source=./helpers.sh
 source "$DIR/helpers.sh"
 
+# shellcheck source=./.exports
+source "$DIR"/.exports
+
+config_path="${XDG_CONFIG_HOME:-"$HOME"/.config}/git"
+
+if [ -e "$HOME"/.gitconfig ]; then
+  message "Migrating to new configuration file location..."
+  mkdir -p "$config_path"
+  mv "$HOME"/.gitconfig "$config_path"/config
+fi
+
 message "Setting up Git..."
 # If gitconfig does not exist already, create one
-if [ ! -e "$HOME"/.gitconfig ]; then
-    message "  %s" "Copying gitconfig to home directory..."
-    cp "$DIR/.gitconfig" ~
+if [ ! -e "$config_path"/config ]; then
+    message "  %s" "Copying gitconfig to '$config_path/config'..."
+    cp "$DIR"/.gitconfig "$config_path"/config
     
     message "  %s" "Configuring Git..."
     read -r -p "  Full name: " NAME
@@ -17,9 +28,9 @@ if [ ! -e "$HOME"/.gitconfig ]; then
     read -r -p "  Github username: " GITHUB
 
     # Only set values if they are non-empty
-    [[ -n "$NAME" ]] && git config --global user.name "$NAME"
-    [[ -n "$EMAIL" ]] && git config --global user.email "$EMAIL"
-    [[ -n "$GITHUB" ]] && git config --global github.user "$GITHUB"
+    [ -n "$NAME" ] && git config --global user.name "$NAME"
+    [ -n "$EMAIL" ] && git config --global user.email "$EMAIL"
+    [ -n "$GITHUB" ] && git config --global github.user "$GITHUB"
 
     if "$DIR"/bin/is-wsl; then
       git config --global credential.helper \
