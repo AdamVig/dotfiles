d4d286b934c6462ede314938446197607856e4d7
Adam Vigneaux <adam@adamvig.com>
2019-05-17 20:04:07 -0400

    Fix golint URL

--- golang.sh
+++ golang.sh
@@ -13,7 +13,6 @@ go_packages=(
     github.com/derekparker/delve/cmd/dlv  # Debugger
     github.com/fatih/gomodifytags  # Modify/update field tags in structs
     github.com/genuinetools/udict  # Urban Dictionary CLI
-    github.com/golang/lint/golint  # Go linter
     github.com/josharian/impl  # Generate method stubs for an interface
     github.com/nsf/gocode  # Autocomplete
     github.com/ramya-rao-a/go-outline  # Extract Go declarations as JSON
@@ -21,6 +20,7 @@ go_packages=(
     github.com/rogpeppe/godef  # Print where symbols are defined
     github.com/sqs/goreturns  # Add zero values to return statements to save time
     github.com/tpng/gopkgs  # Faster `go list all`
+    golang.org/x/lint/golint  # Go linter
     golang.org/x/tools/cmd/godoc  # Go documentation tool
     golang.org/x/tools/cmd/gorename  # Rename identifiers
     golang.org/x/tools/cmd/guru  # Answers questions about Go code
