012f3e8c8e602a1ae2cf2169bb86c57d6a969b6d
Adam Vigneaux <adam@adamvig.com>
2019-08-02 11:01:17 -0400

    Install Tmux Plugin Manager in XDG data dir

--- tmux.sh
+++ tmux.sh
@@ -17,9 +17,9 @@ ln -sf "$DIR"/tmux.conf "$config_path"
 message "done initializing Tmux configuration."
 
 message "initializing Tmux Plugin Manager..."
-readonly tpm_path="$HOME/.tmux/plugins/tpm"
+readonly tpm_path="$(xdg_data)"/tmux/plugins/tpm
 if ! [ -d "$tpm_path" ]; then
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm "$tpm_path"
   message "done initializing Tmux Plugin Manager."
 else
   message "Tmux Plugin Manager already initialized."
