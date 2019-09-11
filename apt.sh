28fa2441e129147b097e4d457498f4680b0fa373
Adam Vigneaux <adam@adamvig.com>
2019-09-11 11:18:14 -0400

    Install xdg-utils on Ubuntu

--- apt.sh
+++ apt.sh
@@ -10,4 +10,8 @@ message "Setting up apt..."
 request-sudo apt-get update > /dev/null
 request-sudo apt-get upgrade -y > /dev/null
 
+message '  %s' 'Installing xdg-utils...'
+sudo apt install -y xdg-utils
+message '  %s' 'Done installing xdg-utils.'
+
 message "Done setting up apt."
