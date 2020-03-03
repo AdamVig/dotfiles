88d8295b44dcabaeebbaca4a6ee857eec7330f8e
Adam Vigneaux <adam@adamvig.com>
2020-03-03 09:41:34 -0600

    Fix formatting

--- pip.sh
+++ pip.sh
@@ -5,16 +5,16 @@ DIR="$(dirname "$(realpath "$0")")"
 # shellcheck source=./helpers.sh
 source "$DIR/helpers.sh"
 
-message "Installing pip tools..."
+message "installing pip tools..."
 pip_packages=(
-    csvkit    # CSV processing tools
-    pip
-    grip    # GitHub README instant preview
-    visidata   # Data set visualizer
+  csvkit # CSV processing tools
+  pip # Package manager
+  grip # GitHub README instant preview
+  visidata # Data set visualizer
 )
 
 for package in "${pip_packages[@]}"; do
-    pip3 install --upgrade --user "$package" > /dev/null
-    message "  %s" "Installed $package"
+  pip3 install --upgrade --user "$package" > /dev/null
+  message "  %s" "installed $package"
 done
-message "Pip done."
+message "pip done."
