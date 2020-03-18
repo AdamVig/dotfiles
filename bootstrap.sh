67e9a6d468e7b75bc53b8b50815f7ff350c7b6f3
Adam Vigneaux <adam@adamvig.com>
2020-03-18 10:16:07 -0500

    Add Windows Terminal configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -72,6 +72,12 @@ fi
 if "$DIR"/bin/is-wsl; then
   message "Copying WSL configuration..."
   request-sudo cp "$DIR"/wsl.conf /etc/wsl.conf
+
+  message "Copying Windows Terminal configuration..."
+  readonly appdata_local_path="$(wslpath "$("$DIR"/bin/expand-windows-path %APPDATALOCAL%)")"
+  windows_terminal_path="$appdata_local_path"/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/
+  mkdir -p "$windows_terminal_path"
+  cp windows-terminal-profiles.json "$windows_terminal_path"/profiles.json
 fi
 
 if ! command -v lsix > /dev/null; then
