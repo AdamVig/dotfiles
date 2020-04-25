55eeec9731a37228421b806fa43496b2bfda51ac
Adam Vigneaux <adam@adamvig.com>
2020-04-25 14:25:23 -0500

    Enable Homebrew Emacs service on macOS

--- brew.sh
+++ brew.sh
@@ -16,4 +16,13 @@ if ! brew bundle install --no-lock; then
     warn "installing Brew formulas may have failed"
 fi
 
+if "$DIR"/bin/is-macos && brew services list | grep --quiet --extended-regexp 'emacs .+ stopped'; then
+    message "magenta" "  %s" "Enabling Emacs service..."
+    if brew services start emacs; then
+        message "magenta" "  %s" "Enabled Emacs service."
+    else
+        warn "Failed to enable Emacs service."
+    fi
+fi
+
 message "magenta" "Brew done."
