0b990506b11409d0d9cfa688898d2f41bab07413
Adam Vigneaux <adam@adamvig.com>
2020-04-27 09:46:54 -0500

    Use correct variable for local app data

--- wsl.sh
+++ wsl.sh
@@ -5,13 +5,15 @@ DIR="$(dirname "$(realpath "$0")")"
 # shellcheck source=./helpers.sh
 source "$DIR/helpers.sh"
 
+request-sudo
+
 message "setting up WSL..."
 
 message "  %s" "copying WSL configuration..."
 request-sudo cp "$DIR"/wsl.conf /etc/wsl.conf
 
 message "  %s" "copying Windows Terminal configuration..."
-readonly appdata_local_path="$(wslpath "$("$DIR"/bin/expand-windows-path %APPDATALOCAL%)")"
+readonly appdata_local_path="$(wslpath "$("$DIR"/bin/expand-windows-path %LOCALAPPDATA%)")"
 windows_terminal_path="$appdata_local_path"/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/
 mkdir -p "$windows_terminal_path"
 cp "$DIR"/windows-terminal-profiles.json "$windows_terminal_path"/profiles.json
