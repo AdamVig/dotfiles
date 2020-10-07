375ce10f5bec2ff6615a548d669af6077eda2cda
Adam Vigneaux <adam@adamvig.com>
2020-10-07 09:10:43 -0500

    Set font and font size

--- config/emacs.el
+++ config/emacs.el
@@ -22,7 +22,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+	)
+
+;; Set the font and font size (in 1/10pt)
+(set-face-attribute 'default nil :font "Fira Code" :height 150)
 
 ;; Install and set up EditorConfig (https://github.com/editorconfig/editorconfig-emacs)
 (use-package editorconfig
