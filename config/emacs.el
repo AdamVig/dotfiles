8c191b734e531b9a0d990a77f4d5805edc3dba4a
Adam Vigneaux <adam@adamvig.com>
2020-11-03 15:35:50 -0600

    Increase max commit subject length

--- config/emacs.el
+++ config/emacs.el
@@ -14,8 +14,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(unicode-fonts i3wm-config-mode writegood-mode markdown-mode magit add-node-modules-path prettier-js git-commit yaml-mode go-mode atom-one-dark-theme editorconfig use-package)))
+	'(git-commit-summary-max-length 72)
+	'(package-selected-packages
+		 '(unicode-fonts i3wm-config-mode writegood-mode markdown-mode magit add-node-modules-path prettier-js git-commit yaml-mode go-mode atom-one-dark-theme editorconfig use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
