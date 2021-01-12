ce4c666cc9be23884e7037715558b30f0e0dcdc2
Adam Vigneaux <adam@adamvig.com>
2021-01-12 10:39:46 -0600

    Change Org archive name suffix to use a hyphen

--- config/emacs.el
+++ config/emacs.el
@@ -22,8 +22,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(git-commit-summary-max-length 72)
- '(package-selected-packages
-   '(comapny tide ox-gfm unicode-fonts i3wm-config-mode writegood-mode markdown-mode magit add-node-modules-path prettier-js git-commit yaml-mode go-mode atom-one-dark-theme editorconfig use-package)))
+ '(org-archive-location "%s-archive::")
+	'(package-selected-packages
+		 '(comapny tide ox-gfm unicode-fonts i3wm-config-mode writegood-mode markdown-mode magit add-node-modules-path prettier-js git-commit yaml-mode go-mode atom-one-dark-theme editorconfig use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
