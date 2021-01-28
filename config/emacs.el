2efa78dda657fecd889ffe6e5177160bd1ab6a5d
Adam Vigneaux <adam@adamvig.com>
2021-01-28 13:43:35 -0600

    Fix custom-set-variables indentation

--- config/emacs.el
+++ config/emacs.el
@@ -25,8 +25,8 @@
  '(org-archive-location "%s-archive::")
  '(org-babel-load-languages '((emacs-lisp . t) (shell . t)))
  '(org-confirm-babel-evaluate nil)
-	'(package-selected-packages
-		 '(company tide ox-gfm unicode-fonts i3wm-config-mode writegood-mode markdown-mode magit add-node-modules-path prettier-js git-commit yaml-mode go-mode atom-one-dark-theme editorconfig use-package)))
+ '(package-selected-packages
+		'(company tide ox-gfm unicode-fonts i3wm-config-mode writegood-mode markdown-mode magit add-node-modules-path prettier-js git-commit yaml-mode go-mode atom-one-dark-theme editorconfig use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
