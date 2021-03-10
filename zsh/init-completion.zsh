8fce33f5394866aa4fb9c997835b1d3a2d04b5df
Adam Vigneaux <adam@adamvig.com>
2021-03-09 20:20:04 -0600

    Source hass-cli completion after running compinit

--- zsh/init-completion.zsh
+++ zsh/init-completion.zsh
@@ -18,10 +18,6 @@ if [ -d "$gh_completion_path" ] && [[ "$FPATH" != *"$gh_completion_path"* ]]; th
 	fpath=("$gh_completion_path" $fpath)
 fi
 
-if command -v hass-cli >/dev/null; then
-	source <(hass-cli completion zsh)
-fi
-
 zmodload -i zsh/complist
 autoload -U compinit
 compinit -i -d "${XDG_CACHE_HOME:-$HOME/.cache}"/zcompdump
@@ -29,3 +25,7 @@ compinit -i -d "${XDG_CACHE_HOME:-$HOME/.cache}"/zcompdump
 zstyle ':completion:*' menu select
 
 compdef _git log-branch-commits=git-branch
+
+if command -v hass-cli >/dev/null; then
+	source <(hass-cli completion zsh)
+fi
