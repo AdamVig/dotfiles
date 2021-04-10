43cbcf3cc59ec0522d4b9decb7f34d0adaea32ea
Adam Vigneaux <adam@adamvig.com>
2021-04-09 22:34:52 -0500

    Cache completion/security checks
    This saves ~8ms in shell startup.

--- zsh/init-completion.zsh
+++ zsh/init-completion.zsh
@@ -19,8 +19,20 @@ if [ -d "$gh_completion_path" ] && [[ "$FPATH" != *"$gh_completion_path"* ]]; th
 fi
 
 zmodload -i zsh/complist
-autoload -U compinit
-compinit -i -d "${XDG_CACHE_HOME:-$HOME/.cache}"/zcompdump
+
+# Initialize completion, ignoring insecure directories and caching for 20 hours
+# Copied from Prezto (https://github.com/sorin-ionescu/prezto/tree/master/modules/completion)
+autoload -Uz compinit
+_comp_path="${XDG_CACHE_HOME:-$HOME/.cache}/zcompdump"
+# #q expands globs in conditional expressions
+if [[ $_comp_path(#qNmh-20) ]]; then
+  # -C (skip function check) implies -i (skip security check)
+  compinit -C -d "$_comp_path"
+else
+  mkdir -p "$_comp_path:h"
+  compinit -i -d "$_comp_path"
+fi
+unset _comp_path
 
 zstyle ':completion:*' menu select
 
