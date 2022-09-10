d979ee181ab8f96b12100fe251008bb837e097cf
Adam Vigneaux <adam@adamvig.com>
2022-09-10 15:46:27 -0500

    Increase allowed commit length

--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -14,6 +14,9 @@ if [[ "$FPATH" != *"zsh-z"* ]]; then
 fi
 
 source "${XDG_DATA_HOME:-$HOME/.local/share}"/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+# Override length after which Git commit message is highlighted in red
+# Default is 50, but GitHub displays up to 72 characters (https://cbea.ms/git-commit/#limit-50)
+FAST_HIGHLIGHT[git-cmsg-len]=71
 
 # Enable built-in functions (see man zshcontrib)
 autoload zcalc
