924e37526d691c799796dd79ace74dd494227266
Adam Vigneaux <adam@adamvig.com>
2024-10-10 16:39:10 -0500

    Add Hammerspoon URL dispatcher

--- config/hammerspoon.lua
+++ config/hammerspoon.lua
@@ -1,3 +1,12 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.alert.show("Hello World!")
-end)
+hs.loadSpoon("URLDispatcher")
+
+spoon.URLDispatcher.url_patterns = {
+    { "https?://.*%.zoom%.us", "us.zoom.xos" },
+    { "https?://.*%.slack%.com", "com.tinyspeck.slackmacgap" },
+    { "https?://linear%.app", "com.linear" },
+    { "https?://www.notion%.so", "notion.id" },
+}
+
+spoon.URLDispatcher.default_handler = "com.google.Chrome"
+
+spoon.URLDispatcher:start()
