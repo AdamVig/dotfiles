8eec85b48ee13eeb536919d1eda7c7eca5fc9b30
Adam Vigneaux <adam@adamvig.com>
2024-11-04 08:23:26 -0600

    Fix Slack and add Figma to Hammerspoon browser patterns

--- config/hammerspoon.lua
+++ config/hammerspoon.lua
@@ -2,9 +2,10 @@ hs.loadSpoon("URLDispatcher")
 
 spoon.URLDispatcher.url_patterns = {
     { "https?://.*%.zoom%.us", "us.zoom.xos" },
-    { "https?://.*%.slack%.com", "com.tinyspeck.slackmacgap" },
+    { "https?://.*%.slack%.com/archives", "com.tinyspeck.slackmacgap" },
     { "https?://linear%.app", "com.linear" },
     { "https?://www.notion%.so", "notion.id" },
+    { "https?://www%.figma%.com", "com.figma.Desktop" },
 }
 
 spoon.URLDispatcher.default_handler = "com.google.Chrome"
