1882a7885260bed40e00e5eb2d03c6c5895ff24e
Adam Vigneaux <adam@adamvig.com>
2024-11-06 14:02:48 -0600

    Fix Slack URL handling

--- config/hammerspoon.lua
+++ config/hammerspoon.lua
@@ -1,11 +1,12 @@
 hs.loadSpoon("URLDispatcher")
 
 spoon.URLDispatcher.url_patterns = {
-    { "https?://.*%.zoom%.us", "us.zoom.xos" },
-    { "https?://.*%.slack%.com/archives", "com.tinyspeck.slackmacgap" },
-    { "https?://linear%.app", "com.linear" },
-    { "https?://www.notion%.so", "notion.id" },
-    { "https?://www%.figma%.com", "com.figma.Desktop" },
+	{ "https?://.*%.zoom%.us", "us.zoom.xos" },
+	-- Do not match URLs with query params; the app can't handle them
+  { "^https?://.+%.slack%.com/archives/[^%?]+$", "com.tinyspeck.slackmacgap" },
+  { "https?://linear%.app", "com.linear" },
+  { "https?://www.notion%.so", "notion.id" },
+  { "https?://www%.figma%.com", "com.figma.Desktop" },
 }
 
 spoon.URLDispatcher.default_handler = "com.google.Chrome"
