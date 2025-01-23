d617567f895a136861f926b5cc12aeadd6452cb9
Adam Vigneaux <adam@adamvig.com>
2025-01-23 08:30:51 -0600

    Refine Zoom/Linear URL patterns

--- config/hammerspoon.lua
+++ config/hammerspoon.lua
@@ -1,10 +1,10 @@
 hs.loadSpoon("URLDispatcher")
 
 spoon.URLDispatcher.url_patterns = {
-	{ "https://.*%.zoom%.us", "us.zoom.xos" },
+	{ "https://.*%.zoom%.us/j", "us.zoom.xos" },
 	-- Do not match URLs with query params; the app can't handle them
   { "https://.+%.slack%.com/archives/[^%?]+$", "com.tinyspeck.slackmacgap" },
-  { "https://linear%.app", "com.linear" },
+  { "https://linear%.app/.*/issue", "com.linear" },
   { "https://www%.notion%.so", "notion.id" },
   { "https://www%.figma%.com/design", "com.figma.Desktop" },
 }
