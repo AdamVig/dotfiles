67da8c2eb71c0ac4ce4504ec85b3b4ce1ea96289
Adam Vigneaux <adam@adamvig.com>
2025-01-23 09:28:21 -0600

    Only include local branches in fzf branch selector
    Also remove apparently-unnecessary logic.

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -47,11 +47,9 @@ _fzf-gf() {
 
 _fzf-gb() {
   is_in_git_repo || return
-  git branch -a --color=always | grep -v '/HEAD\s' | sort |
+  git branch --color=always | grep -v '/HEAD\s' | sort |
   fzf-down --ansi --multi --tac --preview-window right:70% \
-    --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1)' |
-  sed 's/^..//' | cut -d' ' -f1 |
-  sed 's#^remotes/##'
+    --preview 'git log --oneline --graph --date=short --color=always --pretty="format:%C(auto)%cd %h %s" {}'
 }
 
 _fzf-gt() {
