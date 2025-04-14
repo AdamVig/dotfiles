78f953d07b6dde2c6b67ff0371a0f677bc625695
Adam Vigneaux <adam@adamvig.com>
2025-04-14 09:24:01 -0500

    Remove Slack link handler
    Even with the previous tweaks, this will sometimes focus Slack without
    navigating to the message.

--- config/hammerspoon.lua
+++ config/hammerspoon.lua
@@ -2,8 +2,6 @@ hs.loadSpoon("URLDispatcher")
 
 spoon.URLDispatcher.url_patterns = {
 	{ "https://.*%.zoom%.us/j", "us.zoom.xos" },
-	-- Do not match URLs with query params; the app can't handle them
-  { "https://.+%.slack%.com/archives/[^%?]+$", "com.tinyspeck.slackmacgap" },
   { "https://linear%.app/.*/issue", "com.linear" },
   { "https://www%.notion%.so", "notion.id" },
   { "https://www%.figma%.com/design", "com.figma.Desktop" },
