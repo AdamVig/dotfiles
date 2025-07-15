ff4a532293669030310805e333cbc2dc0676a3ee
Adam Vigneaux <adam@adamvig.com>
2025-07-15 09:04:49 -0500

    Disable individual HDMI outputs on laptop
    On my Lenovo P14s, the previous configuration disabled the entire
    built-in audio device, inputs and outputs. This more surgical approach
    disables only the three DisplayPort / HDMI audio outputs.

--- config/wireplumber-disable-devices.lua
+++ config/wireplumber-disable-devices.lua
@@ -3,9 +3,6 @@ alsa_monitor.rules = alsa_monitor.rules or {}
 table.insert(alsa_monitor.rules, {
   matches = {
     {
-      { "device.name", "equals", "alsa_card.pci-0000_00_1f.3-platform-skl_hda_dsp_generic" },
-    },
-    {
       { "device.name", "equals", "alsa_card.usb-CalDigit__Inc._CalDigit_Thunderbolt_3_Audio-00" },
     },
     {
@@ -14,5 +11,22 @@ table.insert(alsa_monitor.rules, {
   },
   apply_properties = {
     ["device.disabled"] = true,
+  }
+})
+
+table.insert(alsa_monitor.rules, {
+  matches = {
+    {
+      { "node.name", "equals", "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_5__sink" },
+    },
+    {
+      { "node.name", "equals", "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_4__sink" },
+    },
+    {
+      { "node.name", "equals", "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__hw_sofhdadsp_3__sink" },
+    },
+  },
+  apply_properties = {
+    ["node.disabled"] = true,
   }
 })
